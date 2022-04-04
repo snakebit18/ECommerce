8aa8e1c2e693ddb7021c290199efa3272054cf10
snakebit18 <sanketbandgar01@gmail.com>
2022-04-04 12:53:46 +0530

    1st-commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function App() {
+  return (
+    <div >
+    <header>
+
+ <a href="/">Amazon</a>
+    </header>
+    <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
