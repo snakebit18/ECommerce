2c9e8abccb473680c82fa3d93793900a4ab2879d
snakebit18 <sanketbandgar01@gmail.com>
2022-04-04 13:35:46 +0530

    2nd-commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React from "react";
+import data from "./data";
 function App() {
   return (
-    <div >
-    <header>
-
- <a href="/">Amazon</a>
-    </header>
-    <main>list products</main>
+    <div>
+      <header>
+        <a href="/">Amazon</a>
+      </header>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`product/>${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`product/>${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
